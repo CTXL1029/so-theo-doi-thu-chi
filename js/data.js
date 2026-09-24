8edc22ba238b9bf4b8e3adf23eba7d456a07d71b
Thanh Tran Cong <110585319+CTXL1029@users.noreply.github.com>
2026-09-24 23:03:33 +0700

    Thêm giao dịch #260924-005130341

--- js/data.js
+++ js/data.js
@@ -1,5 +1,14 @@
 const transactionsData = [
   {
+    "id": "260924-005130341",
+    "type": "thu",
+    "datetime": "2026-09-24T22:32:00",
+    "title": "Phụ huynh gửi tiền quỹ lớp",
+    "content": "Phụ huynh NGUYEN THI NGUYET đã gửi tiền quỹ lớp",
+    "amount": "100.000 VNĐ",
+    "receiptUrl": "assets/2026-09/proof/260924-005130341.png"
+  },
+  {
     "id": "260924-005062661",
     "type": "thu",
     "datetime": "2026-09-24T22:18:00",
